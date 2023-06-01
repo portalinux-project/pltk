32951a4fe5e00fd6144dd945bbdb3804508b3c7a
pocketlinux32 <pocketlinux32@gmail.com>
2023-06-01 00:43:15 -0500

    made it a bit easier to clean up the build directory

--- compile.sh
+++ compile.sh
@@ -3,11 +3,11 @@
 
 set -e
 clang -Iinclude -g -shared -fPIC -lpl32 -lm src/pltk-backend-fbdev.c -o libpltk-backend.so
-clang -Iinclude -g -L. -lpltk-backend pltk-test.c -o pltk-test
+clang -Iinclude -g -L. -lpltk-backend pltk-test.c -o pltk-test.out
 
 if [ $(id -u) -ne 0 ]; then
 	echo "To test this program, run this line as root on a Linux framebuffer terminal:"
-	echo "LD_LIBRARY_PATH=. ./pltk-test"
+	echo "LD_LIBRARY_PATH=. ./pltk-test.out"
 else
 	LD_LIBRARY_PATH=. ./pltk-test
 fi
