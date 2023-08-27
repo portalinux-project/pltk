5be3d7ae8a0c0ea26c93ef5d6295c09a41ad4a41
pocketlinux32 <pocketlinux32@gmail.com>
2023-08-27 02:16:53 -0500

    stuff

--- src/backend/fbdev/input.c
+++ src/backend/fbdev/input.c
@@ -313,8 +313,8 @@ pltkevent_t plTKGetInput(pltkinput_t* inputDevice){
 			break;
 	}
 
-	retStruct.time.tv_sec = rawEvent.tv_sec;
-	retStruct.time.tv_nsec = rawEven.tv_usec * 1000;
+	retStruct.time.tv_sec = rawEvent.time.tv_sec;
+	retStruct.time.tv_nsec = rawEvent.time.tv_usec * 1000;
 	return retStruct;
 }
 
